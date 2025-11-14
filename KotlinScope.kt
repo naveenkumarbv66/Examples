6d6bb134b455463b031bb3d06be55dd4c520eedd
Naveen Kumar BV <naveenkumarbv66@gmail.com>
2025-11-15 00:09:40 +0530

    Koltin Scope

---
+++ KotlinScope.kt
@@ -0,0 +1,43 @@
+package com.naveen.testing
+
+
+fun main(){
+    println("Hello")
+
+    val num = 5
+        .also { println("Also 1: $it") }
+        .let {
+            println("Let: $it")
+            it + 5
+        }
+        .also {
+            println("Also 2: $it")
+            it + it
+        }
+        .run {
+            println("Run: $this")
+            this + 10
+        }
+        .apply {
+            println("Apply: $this")
+            this + this
+        }
+
+    with(num){
+        println("with: $this")
+        this * this
+    }
+
+    println(num)
+
+    /*
+        Hello
+        Also 1: 5
+        Let: 5
+        Also 2: 10
+        Run: 10
+        Apply: 20
+        with: 20
+        20
+     */
+}
